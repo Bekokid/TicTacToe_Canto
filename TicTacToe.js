e53a5eef65bf210dee29e7a5203bcd9335a29391
Bekokid <pei.sern.87@gmail.com>
2022-10-13 01:49:12 +0800

    Update TicTacToe.js

--- TicTacToe.js
+++ TicTacToe.js
@@ -1118,6 +1118,20 @@ function checkNpcWinning() {
       "https://imgs.qiubiaoqing.com/qiubiaoqing/imgs/625b163c7adb9mnN.gif";
     document.getElementById("b6").src =
       "https://imgs.qiubiaoqing.com/qiubiaoqing/imgs/625b163c7adb9mnN.gif";
+  } else if (
+    (b1 == "0" || b1 == "X") &&
+    (b2 == "0" || b2 == "X") &&
+    (b3 == "0" || b3 == "X") &&
+    (b4 == "0" || b4 == "X") &&
+    (b5 == "0" || b5 == "X") &&
+    (b6 == "0" || b6 == "X") &&
+    (b7 == "0" || b7 == "X") &&
+    (b8 == "0" || b8 == "X") &&
+    (b9 == "0" || b9 == "X")
+  ) {
+    document.getElementById("print").innerHTML = "平局";
+    document.getElementById("turnEnd").className = "hidden";
+    document.getElementById("but").className = "show";
   }
 }
 
