e1000b0aadfdb3999c22824a27348bb3b219483e
Bekokid <pei.sern.87@gmail.com>
2022-10-13 01:35:48 +0800

    Update TicTacToe.js

--- TicTacToe.js
+++ TicTacToe.js
@@ -981,24 +981,6 @@ function npcStrategy() {
   console.log(flag);
 }
 
-function checkEnding() {
-  while (
-    (b1 == "0" || b1 == "X") &&
-    (b2 == "0" || b2 == "X") &&
-    (b3 == "0" || b3 == "X") &&
-    (b4 == "0" || b4 == "X") &&
-    (b5 == "0" || b5 == "X") &&
-    (b6 == "0" || b6 == "X") &&
-    (b7 == "0" || b7 == "X") &&
-    (b8 == "0" || b8 == "X") &&
-    (b9 == "0" || b9 == "X")
-  ) {
-    document.getElementById("print").innerHTML = "平局";
-    document.getElementById("turnEnd").className = "hidden";
-    document.getElementById("but").className = "show";
-  }
-}
-
 function checkNpcWinning() {
   if (b1 == "0" && b2 == "0" && b3 == "0") {
     document.getElementById("print").innerHTML = "花輪同學獲勝～！";
@@ -1136,5 +1118,23 @@ function checkNpcWinning() {
       "https://imgs.qiubiaoqing.com/qiubiaoqing/imgs/625b163c7adb9mnN.gif";
     document.getElementById("b6").src =
       "https://imgs.qiubiaoqing.com/qiubiaoqing/imgs/625b163c7adb9mnN.gif";
+  }
+}
+
+function checkEnding() {
+  while (
+    (b1 == "0" || b1 == "X") &&
+    (b2 == "0" || b2 == "X") &&
+    (b3 == "0" || b3 == "X") &&
+    (b4 == "0" || b4 == "X") &&
+    (b5 == "0" || b5 == "X") &&
+    (b6 == "0" || b6 == "X") &&
+    (b7 == "0" || b7 == "X") &&
+    (b8 == "0" || b8 == "X") &&
+    (b9 == "0" || b9 == "X")
+  ) {
+    document.getElementById("print").innerHTML = "平局";
+    document.getElementById("turnEnd").className = "hidden";
+    document.getElementById("but").className = "show";
   }
 }
