dd4238cbe639145d4718dc874f33f83098f156f1
Bekokid <pei.sern.87@gmail.com>
2022-10-13 13:37:43 +0800

    Update TicTacToe.js

--- TicTacToe.js
+++ TicTacToe.js
@@ -1192,6 +1192,16 @@ function checkEnding() {
     (b8 == "0" || b8 == "X") &&
     (b9 == "0" || b9 == "X")
   ) {
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b3").disabled = true;
+    document.getElementById("b4").disabled = true;
+    document.getElementById("b5").disabled = true;
+    document.getElementById("b6").disabled = true;
+    document.getElementById("b7").disabled = true;
+    document.getElementById("b8").disabled = true;
+    document.getElementById("b9").disabled = true;
+
     document.getElementById("print").innerHTML = "平局";
     document.getElementById("turnEnd").className = "hidden";
     document.getElementById("but").className = "show";
